c1e805143271835690c85cfd22c519d4de052939
amarolahy <ramah.samuel@gmail.com>
2020-06-28 09:39:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Interactive signup
+Implementation of an interactive sign-up page from the dribble shot: https://dribbble.com/shots/12209934-Signup-Interaction
+
+## Install
+`npm install`
+
+## Build for dev
+`npm run dev`
+
+## Build for prod
+`npm run build`
+
+## Start server
+`npm run start`
+
+Open your browser at http://localhost:8080/
---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/ts/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
+      { test: /\.tsx?$/, loader: "babel-loader" },
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
